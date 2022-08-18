28fee06d45a446eb0fbc76f1d3e6b43dd2d30a86
mvaldesm <mvaldesm@github.com>
2022-08-17 23:45:44 -0400

    Desafío 2 ReactJS - NavBarJS

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Navbar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Desafío 1 - Curso ReactJS</p>
-        <a
-          className="App-link"
-          href="https://coderhouse.cl"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Ir a CoderHouse
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Desafío 2 - Curso ReactJS</p>
+          <a
+            className="App-link"
+            href="https://coderhouse.cl"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Ir a CoderHouse
+          </a>
+        </header>
+      </div>
+    </>
   );
 }
 
